2a09ffb6a959120032d988849adbd3ed3cfd5b2e
Sabareesh18053 <69257069+Sabareesh18053@users.noreply.github.com>
2022-10-06 17:30:45 +0530

    fisrt commit

---
+++ src/Pages/admin.js
@@ -0,0 +1,12 @@
+
+
+function Admin(){
+
+    return (
+    <div className='container mx-auto pb-24'>
+        <h1 className='text-lg font-bold my-8'>Admin Access page</h1>
+       
+    </div>
+  );
+}
+export default Admin;
